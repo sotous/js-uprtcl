23e0525400db8ef341801955e7df30f63012bfba
Jesus Soto <sotous0@gmail.com>
2021-07-05 09:28:09 -0400

    Exported some needed modules to be used when package is called

--- remotes/http/src/uprtcl-evees-http.ts
+++ remotes/http/src/uprtcl-evees-http.ts
@@ -1,4 +1,6 @@
-export { EveesHttp } from './provider/evees.http.client.remote';
-export { EveesAccessControlHttp } from './provider/evees-acl.http';
-export { EveesHttpModule } from './evees-http.module';
+export { HttpEntityRemote } from './provider/evees.http.entity.remote';
+export { ProposalsHttp } from './provider/proposals.http';
 export { PermissionType } from './provider/types';
+export { UserPermissions } from './provider/types';
+export { BasicAdminPermissions } from './provider/types';
+export { BasicAdminInheritedPermissions } from './provider/types';
