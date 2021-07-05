df71d8942785fca14127b40982d00258527232ab
Jesus Soto <sotous0@gmail.com>
2021-07-05 09:26:01 -0400

    Created apart another package for evees-http UI

---
+++ remotes/http-ui/README.md
@@ -0,0 +1,15 @@
+# @uprtcl/evees-http-ui
+
+TBW
+
+## Documentation
+
+Visit our [documentation site](https://uprtcl.github.io/js-uprtcl).
+
+## Install
+
+TBW
+
+## Usage
+
+TBW
---
+++ remotes/http-ui/src/uprtcl-evees-http-ui.ts
@@ -0,0 +1,3 @@
+export { EveesHttpUI } from './provider/evees.http.client.remote';
+export { EveesAccessControlHttp } from './provider/evees-acl.http';
+export { EveesHttpModule } from './evees-http-ui.module';
