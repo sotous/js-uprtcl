fa1da83c560a78ef108d01a49a77d45eeddb4398
Jesus Soto <sotous0@gmail.com>
2021-07-13 08:17:45 -0400

    Modified EthConnection to adapt to new auth abstraction

--- connections/http/src/auth/http.eth.connection.ts
+++ connections/http/src/auth/http.eth.connection.ts
@@ -2,7 +2,7 @@ import { HttpAuthenticatedConnectionImp } from '../http.auth.connection.imp';
 import { HttpEthToken } from './http-eth-auth.token';
 
 export class HttpEthConnection extends HttpAuthenticatedConnectionImp {
-  constructor(host: string) {
-    super(host, new HttpEthToken(host), 'ETH_AUTH_TOKEN', 'ETH_USER_ID');
+  constructor(host: string, authentication: HttpEthToken) {
+    super(host, authentication, 'ETH_AUTH_TOKEN', 'ETH_USER_ID');
   }
 }
