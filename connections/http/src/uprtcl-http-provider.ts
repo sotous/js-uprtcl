82f930240e9450693a78d1d0d3391f7801e51142
Jesus Soto <sotous0@gmail.com>
2021-07-13 08:18:29 -0400

    Exported new classes

--- connections/http/src/uprtcl-http-provider.ts
+++ connections/http/src/uprtcl-http-provider.ts
@@ -4,7 +4,8 @@ export { HttpAuthenticatedConnectionImp } from './http.auth.connection.imp';
 export { HttpAuthenticatedConnection } from './http.auth.connection.if';
 export { HttpMultiConnection } from './http.multi.connection';
 
-export { HttpEthToken } from './auth/http-eth-auth.token';
+export { HttpEthTokenServer } from './auth/http-eth-auth.token.server';
+export { HttpEthTokenBrowser } from './auth/http-eth-auth.token.browser';
 export { HttpEthConnection } from './auth/http.eth.connection';
 
 export { HttpAuth0Token } from './auth/http-auth0.token';
